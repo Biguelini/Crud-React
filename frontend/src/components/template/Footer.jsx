1a5155cf6ee48dadbb6312408ebc1d41023fde73
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-07-02 15:55:23 -0300

    update

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import './Footer.css'
+import React from 'react'
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Biguelini</strong>
+        </span>
+    </footer>
